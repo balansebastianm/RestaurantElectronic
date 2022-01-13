dd78dd3b3a5e7a26cec266c4e3d319987e624c63
MrJ33F <groza.cosminel@gmail.com>
2022-01-13 17:26:36 +0200

    Add project files.

---
+++ RestaurantElectronic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestaurantElectronic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
